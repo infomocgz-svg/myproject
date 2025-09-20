1f6d64cf730165f85729f04b36321d3d57556302
Hnedo <hnedo@example.com>
2025-09-20 23:03:39 +0300

    Оновлено ConsoleApplication1.cpp: додано повідомлення

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,6 +6,7 @@ int main()
 	double x; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
+	cout << "My program" << endl;
 	cout << "x = "; cin >> x;
 	z1 = 2 * x;
 	z2 = x + x;
